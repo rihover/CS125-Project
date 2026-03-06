10be1045a00fce820dd9882c12bcbfc92661a631
huffmanek <huffmanek@gmail.com>
2026-03-06 11:27:43 -0700

    Update 1 FARM.c

--- FARM.c
+++ FARM.c
@@ -2,6 +2,7 @@
 * Authors: Rihanna Overbaugh and Elizabeth Huffman
 * File: FARM.c
 * Version: 1.0 3/5/26 (Started reading in player view of farm)
+*          1.1 3/6/26 (Begin turning it into function that will read any of our ASCII art when called)
 ****************************************************/
 
 #include<stdio.h>
