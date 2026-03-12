7d5e98a26a3a3143bb54b4481bf62ce056cf3a7c
rihover <rihover@gmail.com>
2026-03-12 11:46:49 -0700

    Create plantSeeds.c

---
+++ plantSeeds.c
@@ -0,0 +1,25 @@
+/*****************************************************
+ * Authors: Rihanna Overbaugh and Elizabeth Huffman
+ * File Name: plantSeeds.c
+ * Version: 1.0 3/5/26 
+            1.1 3/12/26
+*****************************************************/
+#include <stdio.h> 
+
+int checkInventory() 
+{
+  if (tomatoSeeds > 0){
+  }
+  if (wildFlowerSeeds > 0){ 
+  }
+  if (radishSeeds > 0){ 
+  }
+  if (sugarCaneSeeds > 0){
+  }
+  if (potatoSeeds > 0){ 
+  } 
+
+
+int main() 
+{
+  
