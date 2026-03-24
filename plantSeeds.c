4bc4112ca5cd7f2e4fd7bc270dab206a4ff177dc
rihover <rihover@gmail.com>
2026-03-24 11:34:07 -0700

    Update plantSeeds.c
    I have yet to see if this works for main

--- plantSeeds.c
+++ plantSeeds.c
@@ -3,23 +3,100 @@
  * File Name: plantSeeds.c
  * Version: 1.0 3/5/26 
             1.1 3/12/26
+            1.2 3/24/26
 *****************************************************/
 #include <stdio.h> 
 
-int checkInventory() 
-{
-  if (tomatoSeeds > 0){
-  }
-  if (wildFlowerSeeds > 0){ 
-  }
-  if (radishSeeds > 0){ 
-  }
-  if (sugarCaneSeeds > 0){
-  }
-  if (potatoSeeds > 0){ 
-  } 
+char plant;
 
+int plantSeed_error(char plant)
+{ 
+  while (plant =! 'n') && (plant =! 'y') 
+  { 
+    printf("Let's try again, would you like to plant? y or n: \n");
+    scanf("%c", &plant);
+  } 
+  return plant;
+ }  
 
 int main() 
 {
+  int tomato, wildFlower, radish, sugarCane, potato; 
   
+  if (tomatoSeeds > 0)
+  {
+    printf("Would you like to plant tomatoes? y or n: \n");
+    scanf("%c", &plant);
+    plant = plantSeeds_error(plant);
+    if (plant == 'y') 
+    {
+      printf("Tomato seeds planted! \n"); 
+      tomato++; 
+    }
+    else if (plant == 'n')
+    { 
+      printf("Tomato seeds not planted! \n");
+    }
+  }
+  if (wildFlowerSeeds > 0)
+  {
+    printf("Would you like to plant Wild Flowers? y or n: \n");
+    scanf("%c", &plant);
+    plant = plantSeeds_error(plant);
+    if (plant == 'y') 
+    {
+      printf("Wild Flower seeds planted! \n"); 
+      wildFlower++; 
+    }
+    else if (plant == 'n')
+    { 
+      printf("Wild Flower seeds not planted! \n");
+    }
+  }
+  if (radishSeeds > 0)
+  {
+    printf("Would you like to plant radishes? y or n: \n");
+    scanf("%c", &plant);
+    plant = plantSeeds_error(plant);
+    if (plant == 'y') 
+    {
+      printf("Radish seeds planted! \n"); 
+      radish++; 
+    }
+    else if (plant == 'n')
+    { 
+      printf("Radish seeds not planted! \n");
+    }
+  }
+  if (sugarCaneSeeds > 0)
+  {
+    printf("Would you like to plant Sugar Cane? y or n: \n");
+    scanf("%c", &plant);
+    plant = plantSeeds_error(plant);
+    if (plant == 'y') 
+    {
+      printf("Sugar Cane seeds planted! \n"); 
+      sugarCane++; 
+    }
+    else if (plant == 'n')
+    { 
+      printf("Sugar Cane seeds not planted! \n");
+    }
+  }
+  if (potatoSeeds > 0)
+  { 
+    printf("Would you like to plant Potatoes? y or n: \n");
+    scanf("%c", &plant);
+    plant = plantSeeds_error(plant);
+    if (plant == 'y') 
+    {
+      printf("Potato seeds planted! \n"); 
+      potato++; 
+    }
+    else if (plant == 'n')
+    { 
+      printf("Potato seeds not planted! \n");
+    }
+  }
+  return 0; 
+}
