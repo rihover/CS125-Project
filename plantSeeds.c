01b67bfee881e3e2884b3a58ecb636c3697b31f9
rihover <rihover@gmail.com>
2026-04-04 13:51:56 -0700

    Update plantSeeds.c

--- plantSeeds.c
+++ plantSeeds.c
@@ -1,14 +1,18 @@
 /*****************************************************
  * Authors: Rihanna Overbaugh and Elizabeth Huffman
  * File Name: plantSeeds.c
- * Version: 1.0 3/5/26 
-            1.1 3/12/26
-            1.2 3/24/26
+ * Version: 1.0 3/5/26 (created file)
+            1.1 3/12/26 (if statements)
+            1.2 3/24/26 (integrating with main)
+            1.4 3/31/26 (debugging)
+            1.5 4/1/26 (more debugging) 
+            1.6 4/4/26 (made seeds pointers and the planted structure) 
 *****************************************************/
 #include <stdio.h>
 #include "plantSeeds.h"
 
 char plant;
+Planted planted;
 
 char plantSeed_error(char plant)
 { 
@@ -20,11 +24,9 @@ char plantSeed_error(char plant)
   return plant;
  }  
 
-void plantSeeds(int tomatoSeeds, int wildFlowerSeeds, int radishSeeds, int sugarCaneSeeds, int potatoSeeds) 
+void plantSeeds(int *tomatoSeeds, int *wildFlowerSeeds, int *radishSeeds, int *sugarCaneSeeds, int *potatoSeeds) 
 {
-  int tomato, wildFlower, radish, sugarCane, potato; 
-  
-  if (tomatoSeeds > 0)
+  if (*tomatoSeeds > 0)
   {
     printf("Would you like to plant tomatoes? y or n: \n");
     scanf(" %c", &plant);
@@ -32,14 +34,15 @@ void plantSeeds(int tomatoSeeds, int wildFlowerSeeds, int radishSeeds, int sugar
     if (plant == 'y') 
     {
       printf("Tomato seeds planted! \n"); 
-      tomato++; 
+      planted.tomato++;
+      (*tomatoSeeds--);
     }
     else if (plant == 'n')
     { 
       printf("Tomato seeds not planted! \n");
     }
   }
-  if (wildFlowerSeeds > 0)
+  if (*wildFlowerSeeds > 0)
   {
     printf("Would you like to plant Wild Flowers? y or n: \n");
     scanf(" %c", &plant);
@@ -47,14 +50,15 @@ void plantSeeds(int tomatoSeeds, int wildFlowerSeeds, int radishSeeds, int sugar
     if (plant == 'y') 
     {
       printf("Wild Flower seeds planted! \n"); 
-      wildFlower++; 
+      planted.wildFlower++;
+      (*wildFlowerSeeds--);
     }
     else if (plant == 'n')
     { 
       printf("Wild Flower seeds not planted! \n");
     }
   }
-  if (radishSeeds > 0)
+  if (*radishSeeds > 0)
   {
     printf("Would you like to plant radishes? y or n: \n");
     scanf(" %c", &plant);
@@ -62,14 +66,15 @@ void plantSeeds(int tomatoSeeds, int wildFlowerSeeds, int radishSeeds, int sugar
     if (plant == 'y') 
     {
       printf("Radish seeds planted! \n"); 
-      radish++; 
+      planted.radish++;
+      (*radishSeeds--);
     }
     else if (plant == 'n')
     { 
       printf("Radish seeds not planted! \n");
     }
   }
-  if (sugarCaneSeeds > 0)
+  if (*sugarCaneSeeds > 0)
   {
     printf("Would you like to plant Sugar Cane? y or n: \n");
     scanf(" %c", &plant);
@@ -77,14 +82,15 @@ void plantSeeds(int tomatoSeeds, int wildFlowerSeeds, int radishSeeds, int sugar
     if (plant == 'y') 
     {
       printf("Sugar Cane seeds planted! \n"); 
-      sugarCane++; 
+      planted.sugarCane++;
+      (*sugarCaneSeeds--);
     }
     else if (plant == 'n')
     { 
       printf("Sugar Cane seeds not planted! \n");
     }
   }
-  if (potatoSeeds > 0)
+  if (*potatoSeeds > 0)
   { 
     printf("Would you like to plant Potatoes? y or n: \n");
     scanf(" %c", &plant);
@@ -92,7 +98,8 @@ void plantSeeds(int tomatoSeeds, int wildFlowerSeeds, int radishSeeds, int sugar
     if (plant == 'y') 
     {
       printf("Potato seeds planted! \n"); 
-      potato++; 
+      planted.potato++;
+      (*potatoSeeds--);
     }
     else if (plant == 'n')
     { 
