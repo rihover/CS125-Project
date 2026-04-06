f1d34785420f589f35a5ca2d2a8284849c2ad1b0
huffmanek <huffmanek@gmail.com>
2026-04-05 20:07:28 -0700

    Update merchant.c
    updatedddd

--- merchant.c
+++ merchant.c
@@ -1,180 +1,194 @@
-/***********************************************************
- * File Name: merchant.c
- * Authors: Rihanna Overbaugh and Eilzabeth Huffman
- * Version: 1.0 4/1/26
-************************************************************/
-#include <stdio.h> 
-#include "merchant.h" 
+#include <stdio.h>
+#include "merchant.h"
 
 #define RESET "\033[0m"
 #define YELLOW "\033[33m"
 
-int sell_errorcheck()
+void merchant(int *CATcoins,
+              int *tomatoSeeds, int *wildFlowerSeeds, int *sugarCaneSeeds,
+              int *radishSeeds, int *potatoSeeds,
+              int *tomato, int *wildFlower, int *radish,
+              int *sugarCane, int *potato)
 {
-    while ((sell != 'y') && (sell != 'n'))
-    {
-        printf("Incorrect input try again! \n");
-        printf("Would you like to sell? (y or n) \n");
-        scanf(" %c", &sell);
-    }
-    return 0;
-}
+    int buy_sell;
+    int buyChoice;
+    char continuebuy = 'y';
+    char continuesell = 'y';
+    char sell;
 
+    printf(YELLOW);
+    printf("Welcome to Luna's Emporium!\n");
+    printf("You have %d CAT coins.\n", *CATcoins);
+    printf("Would you like to buy or sell?\n");
+    printf("1. Buy\n");
+    printf("2. Sell\n");
+    scanf("%d", &buy_sell);
 
-void merchant()
-{ 
-  int buy_sell;
-  int buyseeds;
-  char continuebuy = 'y';
-  char sell;
-  char continuesell = 'y';
+    while (buy_sell != 1 && buy_sell != 2)
+    {
+        printf("Incorrect input. Try again.\n");
+        printf("1. Buy\n");
+        printf("2. Sell\n");
+        scanf("%d", &buy_sell);
+    }
 
-  printf(YELLOW); 
-  printf("Welcome to Luna's emporium! Would you like to Buy items or sell? ( 1 to purchase, 2 to sell): \n");
-  scanf("%d", buy_sell);
-  while ((buy_sell != 1) && (buy_sell != 2))
-  {
-    printf("Incorrect input try again!");
-    printf("Would you like to Buy items or sell? ( 1 to purchase, 2 to sell): \n");
-    scanf("%d", buy_sell);
-  }
-  if (buy_sell == 1) 
-  {
-    printf("Options to buy (Enter the corresponding number):\n1: Wild Flower Seeds (5 CAT coins) \n2: Tomato Seeds (10 CAT coins) \n3:Radish Seeds (15 CAT coins) \n4: Sugar Cane Seeds (20 CAT coins) \n5: Potato Seeds (10 CAT coins) \n")
-    scanf("%d", &buyseeds);
-    while (continuebuy = 'y')
+    if (buy_sell == 1)
     {
-        if (buyseeds == 1) 
+        while (continuebuy == 'y' || continuebuy == 'Y')
         {
-            if (CATcoins < 5)
+            printf("\nOptions to buy:\n");
+            printf("1. Wild Flower Seeds (5 CAT coins)\n");
+            printf("2. Tomato Seeds (10 CAT coins)\n");
+            printf("3. Radish Seeds (15 CAT coins)\n");
+            printf("4. Sugar Cane Seeds (20 CAT coins)\n");
+            printf("5. Potato Seeds (10 CAT coins)\n");
+            printf("Enter your choice: ");
+            scanf("%d", &buyChoice);
+
+            if (buyChoice == 1)
             {
-                printf("Not enough CAT coins \n");
+                if (*CATcoins < 5)
+                {
+                    printf("Not enough CAT coins.\n");
+                }
+                else
+                {
+                    printf("Wild Flower Seeds purchase successful.\n");
+                    (*wildFlowerSeeds)++;
+                    *CATcoins -= 5;
+                }
             }
-            else if (CATcoins >= 10) 
+            else if (buyChoice == 2)
             {
-                printf("Wild Flower Seeds Purchase Sucessful \n");
-                (*wildFlowerSeeds)++;
-                CATcoins - 5;
+                if (*CATcoins < 10)
+                {
+                    printf("Not enough CAT coins.\n");
+                }
+                else
+                {
+                    printf("Tomato Seeds purchase successful.\n");
+                    (*tomatoSeeds)++;
+                    *CATcoins -= 10;
+                }
             }
+            else if (buyChoice == 3)
+            {
+                if (*CATcoins < 15)
+                {
+                    printf("Not enough CAT coins.\n");
+                }
+                else
+                {
+                    printf("Radish Seeds purchase successful.\n");
+                    (*radishSeeds)++;
+                    *CATcoins -= 15;
+                }
+            }
+            else if (buyChoice == 4)
+            {
+                if (*CATcoins < 20)
+                {
+                    printf("Not enough CAT coins.\n");
+                }
+                else
+                {
+                    printf("Sugar Cane Seeds purchase successful.\n");
+                    (*sugarCaneSeeds)++;
+                    *CATcoins -= 20;
+                }
+            }
+            else if (buyChoice == 5)
+            {
+                if (*CATcoins < 10)
+                {
+                    printf("Not enough CAT coins.\n");
+                }
+                else
+                {
+                    printf("Potato Seeds purchase successful.\n");
+                    (*potatoSeeds)++;
+                    *CATcoins -= 10;
+                }
+            }
+            else
+            {
+                printf("Invalid choice.\n");
+            }
+
+            printf("You now have %d CAT coins.\n", *CATcoins);
+            printf("Would you like to continue buying? (y/n): ");
+            scanf(" %c", &continuebuy);
         }
-        else if (buyseeds == 2)
-        {
-            if (CATcoins < 10)
-            {
-                printf("Not enough CAT coins \n");
-            }
-            else if (CATcoins >= 10) 
-            {
-                printf("Tomato Seeds Purchase Sucessful \n");
-                (*tomatoSeeds)++;
-                CATcoins - 10
-            }
-        }
-        else if (buyseeds == 3)
-        {
-            if (CATcoins < 15)
-            {
-                printf("Not enough CAT coins \n");
-            }
-            else if (CATcoins >= 15) 
-            {
-                printf("Radish Seeds Purchase Sucessful \n");
-                (*radishSeeds)++;
-                CATcoins - 15
-            }
-        }
-        else if (buyseeds == 4) 
-        {
-            if (CATcoins < 20)
-            {
-                printf("Not enough CAT coins \n");
-            }
-            else if (CATcoins >= 20)
-            {
-                printf("Sugar Cane Seeds Purchase Sucessful \n");
-                (*sugarCaneSeeds)++;
-                CATcoins - 20;
-            }
-        }
-        else if (buyseeds == 5)
-        {
-            if (CATcoins < 10)
-            {
-                printf("Not enough CAT coins \n"); 
-            }
-            else if (CATcoins >= 10)
-            {
-                printf("Potato Seeds Purchanse Sucessfull \n");
-                (*potatoSeeds)++;
-                CATcoins - 10;
-            }
-        }
-        printf("Would you like to continue? ('y' for yes, 'n' for no) \n");
-        scanf(" %c", &continuebuy);
     }
-  }
-  if (buy_sell == 2)
-  { 
-    while (continuesell == 'y')
+    else
     {
-        if (WildFlower > 0)
+        while (continuesell == 'y' || continuesell == 'Y')
         {
-            printf("Would you like to sell Wild Flowers? (y or n): \n");
-            scanf(" %c", &sell);
-            sell_errorcheck();
-            if (sell == 'y')
+            if (*wildFlower > 0)
             {
-            printf("Wild Flowers Sold! \n");
-            wildFlower--;
+                printf("Would you like to sell a Wild Flower for 5 CAT coins? (y/n): ");
+                scanf(" %c", &sell);
+                if (sell == 'y' || sell == 'Y')
+                {
+                    (*wildFlower)--;
+                    *CATcoins += 5;
+                    printf("Wild Flower sold!\n");
+                }
             }
-        }
-        if (Tomato > 0)
-        {
-            printf("Would you like to sell Tomatoes?: \n");
-            scanf(" %c", &sell);
-            sell_errorcheck();
-            if (sell == 'y')
+
+            if (*tomato > 0)
             {
-                printf("Tomatoes Sold! \n");
-                Tomatoes--;
+                printf("Would you like to sell a Tomato for 10 CAT coins? (y/n): ");
+                scanf(" %c", &sell);
+                if (sell == 'y' || sell == 'Y')
+                {
+                    (*tomato)--;
+                    *CATcoins += 10;
+                    printf("Tomato sold!\n");
+                }
             }
-        }
-        if (Radish > 0)
-        {
-            printf("Would you like to sell Radishes?: \n");
-            scanf(" %c", &sell);
-            sell_errorcheck();
-            if (sell == 'y')
+
+            if (*radish > 0)
             {
-                printf("Radishes Sold! \n");
-                Radishes--;
+                printf("Would you like to sell a Radish for 15 CAT coins? (y/n): ");
+                scanf(" %c", &sell);
+                if (sell == 'y' || sell == 'Y')
+                {
+                    (*radish)--;
+                    *CATcoins += 15;
+                    printf("Radish sold!\n");
+                }
             }
-        }
-        if (SugarCane > 0)
-        {
-            printf("Would you like to sell Sugar Cane?: \n");
-            scanf(" %c", &sell);
-            sell_errorcheck();
-            if (sell == 'y')
+
+            if (*sugarCane > 0)
             {
-                printf("Sugar Cane Sold! \n");
-                SugarCane--;
+                printf("Would you like to sell a Sugar Cane for 20 CAT coins? (y/n): ");
+                scanf(" %c", &sell);
+                if (sell == 'y' || sell == 'Y')
+                {
+                    (*sugarCane)--;
+                    *CATcoins += 20;
+                    printf("Sugar Cane sold!\n");
+                }
             }
-        }
-        if (Potatoes > 0)
-        {
-            printf("Would you like to sell Potatoes? \n");
-            scanf(" %c", &sell)
-            sell_errorcheck();
-            if (sell == 'y')
+
+            if (*potato > 0)
             {
-                printf("Potatoes Sold! \n")
-                Potatoes--;
+                printf("Would you like to sell a Potato for 10 CAT coins? (y/n): ");
+                scanf(" %c", &sell);
+                if (sell == 'y' || sell == 'Y')
+                {
+                    (*potato)--;
+                    *CATcoins += 10;
+                    printf("Potato sold!\n");
+                }
             }
+
+            printf("You now have %d CAT coins.\n", *CATcoins);
+            printf("Would you like to sell again? (y/n): ");
+            scanf(" %c", &continuesell);
         }
-        printf("Would you like to sell again?(y or n): \n");
-        scanf(" %c", &continuesell);
     }
-  }
-  printf(RESET);
+
+    printf(RESET);
 }
